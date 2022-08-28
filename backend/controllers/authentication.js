930e1ec8f80d5053bc70ddaa7e352da068730739
Martin <nitromagix@gmail.com>
2022-08-28 11:07:45 -0700

    finished iam-4-using-sessions-in-rest-rant

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -6,6 +6,28 @@ const router = express.Router();
 
 const { User } = db;
 
+router.get("/profile", async (req, res) => {
+  // console.log(req.session.userId);
+  try {
+    const user = await User.findOne({
+      where: { userId: req.session.userId },
+    });
+    res.json(user);
+  } catch {
+    res.json(null);
+  }
+});
+
+router.post("/super-important-route", async (req, res) => {
+  if (req.session.userId) {
+    console.log("Do the really super important thing");
+    res.send("Done");
+  } else {
+    console.log("You are not authorized to do the super important thing");
+    res.send("Denied");
+  }
+});
+
 router.post("/", async (req, res) => {
   const body = req.body;
 
@@ -14,6 +36,7 @@ router.post("/", async (req, res) => {
   });
 
   if (user && (await bcrypt.compare(body.password, user.passwordDigest))) {
+    req.session.userId = user.userId;
     res.json({ user });
   } else {
     res.status(404).json({
